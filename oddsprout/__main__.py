5c258dca2f7bb8c443ae2df81542842e4d32b61f
trag1c <trag1cdev@yahoo.com>
2024-03-03 11:35:14 +0100

    Added error handling

--- oddsprout/__main__.py
+++ oddsprout/__main__.py
@@ -3,14 +3,19 @@ import sys
 from argparse import ArgumentParser
 from pathlib import Path
 
+from oddsprout.exceptions import OddsproutError
 from oddsprout.generators import generate_value
 
 if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument("--config", type=Path, help="path to configuration file")
     try:
-        print(json.dumps(generate_value(initial=True), indent=2))
-    except RecursionError:
-        print(
-            "Failed to generate a JSON with the given collection size.", file=sys.stderr
-        )
+        parser = ArgumentParser()
+        parser.add_argument("--config", type=Path, help="path to configuration file")
+        try:
+            print(json.dumps(generate_value(initial=True), indent=2))
+        except RecursionError:
+            print(
+                "ERROR: Failed to generate a JSON with the given collection size.",
+                file=sys.stderr,
+            )
+    except OddsproutError as e:
+        sys.exit(f"ERROR: {e}")
