c8b92da870ba144eb6399bb3690fdabca1f216c9
trag1c <trag1cdev@yahoo.com>
2024-03-05 00:54:16 +0100

    Cleaned up __main__.py

--- oddsprout/__main__.py
+++ oddsprout/__main__.py
@@ -2,25 +2,37 @@ import json
 import sys
 from argparse import ArgumentParser
 from pathlib import Path
+from typing import NoReturn
 
-from dahlia import dahlia, dprint
+from dahlia import dahlia
 
 from oddsprout.configuration import load_config
 from oddsprout.exceptions import OddsproutError
-from oddsprout.generators import config, generate_value
+from oddsprout.generators import JSONGenerator
+
+
+def _parse_argv() -> Path:
+    parser = ArgumentParser()
+    parser.add_argument("--config", type=Path, help="path to configuration file")
+    args = parser.parse_args()
+    return args.config
+
+
+def _dexit(message: object) -> NoReturn:
+    sys.exit(dahlia(f"&4ERROR:&r {message}"))
+
 
 if __name__ == "__main__":
+    config_path = _parse_argv()
+    if not config_path.exists():
+        _dexit(f"{config_path} does not exist")
+
     try:
-        parser = ArgumentParser()
-        parser.add_argument("--config", type=Path, help="path to configuration file")
-        args = parser.parse_args()
-        config.update(load_config(args.config))
-        try:
-            print(json.dumps(generate_value(initial=True), indent=2))
-        except RecursionError:
-            dprint(
-                "&4ERROR:&r Failed to generate a JSON with the given collection size.",
-                file=sys.stderr,
-            )
+        json_gen = JSONGenerator(load_config(config_path))
     except OddsproutError as e:
-        sys.exit(dahlia(f"&4ERROR:&r {e}"))
+        _dexit(e)
+
+    try:
+        print(json.dumps(json_gen.generate_value(), indent=2))
+    except RecursionError:
+        _dexit("Failed to generate a JSON with the given collection size.")
