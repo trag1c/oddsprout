dbed76ead32dc83fe4d8a28ffbba5405b55712f1
trag1c <trag1cdev@yahoo.com>
2024-04-06 23:57:11 +0200

    Improved JSONGenerator tests

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -3,7 +3,7 @@ from typing import Literal
 import pytest
 
 from oddsprout.configuration import Config
-from oddsprout.generators import JSONGenerator
+from oddsprout.generators import JSONGenerator, _generate_float, _generate_int
 
 
 @pytest.mark.parametrize("base_type", ["array", "object", "any"])
@@ -12,5 +12,20 @@ def test_json_generator(base_type: Literal["array", "object", "any"]) -> None:
     gen.generate_value()
 
 
-def test_json_generator_no_config() -> None:
-    JSONGenerator()
+def test_json_generator_number_alias() -> None:
+    gen = JSONGenerator(Config(types=("number",)))
+
+    assert set(gen._type_pool) & {_generate_int, _generate_float}
+
+
+def test_json_generator_repr() -> None:
+    gen = JSONGenerator()
+    cfg = gen.config
+
+    assert repr(gen) == f"JSONGenerator(config={cfg})"
+
+
+def test_json_generator_recursion_error() -> None:
+    gen = JSONGenerator(Config(base="object", collection_size=(500, 1000)))
+    with pytest.raises(RecursionError):
+        gen.generate_value()
