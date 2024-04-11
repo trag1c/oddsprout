68fd006e7018f90f53be1909a72e8d2567317cca
trag1c <trag1cdev@yahoo.com>
2024-04-11 23:44:01 +0200

    Exported exceptions in __all__

--- oddsprout/__init__.py
+++ oddsprout/__init__.py
@@ -1,4 +1,17 @@
 from oddsprout.configuration import Config
+from oddsprout.exceptions import (
+    OddsproutConfigurationError,
+    OddsproutError,
+    OddsproutRecursionError,
+    OddsproutValueError,
+)
 from oddsprout.generators import JSONGenerator
 
-__all__ = ("Config", "JSONGenerator")
+__all__ = (
+    "Config",
+    "JSONGenerator",
+    "OddsproutConfigurationError",
+    "OddsproutError",
+    "OddsproutRecursionError",
+    "OddsproutValueError",
+)
